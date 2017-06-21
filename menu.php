366a6a9b46f818863126293e565d93ae6e23be6b
DiogoAvilar <chorofutebolistico@hotmail.com>
2017-06-21 17:23:45 -0300

    pets

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu Responsivo</title>
+	<link rel="icon" href="new-google-favicon-128.png" type="image/png"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+<header>
+<img src="img/logo2.png" alt="" class="logo">
+	<input type="checkbox" id="btn-menu">
+	<label for="btn-menu"><img src="menu-button-of-three-horizontal-lines.svg"></label>
+	<nav class="menu">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Cuidados</a></li>
+			<li><a href="#">Nossos Guardiões</a></li>
+			<li><a href="#">Contato</a></li>
+			<li><a href="#">Formulario</a></li>
+		</ul>
+	</nav>
+</header>
+</body>
+</html>
